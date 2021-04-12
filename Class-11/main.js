0591fc67e9486a0ee4579eeb1a9fad024a57b5e0
MD. Anichur Rahaman <38912435+anis3139@users.noreply.github.com>
2021-04-12 22:41:39 +0600

    Class 11

---
+++ Class-11/main.js
@@ -0,0 +1,31 @@
+/*
+data type
+variable
+conditional statemant
+loop
+function
+object
+array
+*/
+
+// string= sentence ' ' || " ", integer = number, boolean, aboject, undifined, null
+
+var name='Shakib' 
+
+console.log("I Am" + name)
+console.log(name + " go to school")
+console.log(name + " is good boy")
+console.log(name + " is married")
+
+var x=10 ;
+
+var y=20;
+var z=5;
+
+var m=10
+var n=10
+
+var result= m+n
+
+
+console.log(result);
